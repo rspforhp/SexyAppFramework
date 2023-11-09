80cec421795d2538e2bd855ced67764586391b86
Yalek The Lembine <weldonalex3@gmail.com>
2023-11-09 15:23:37 -0500

    revert to first version...

--- source/SexyAppFramework/D3DHelper.h
+++ source/SexyAppFramework/D3DHelper.h
@@ -1,5 +1,5 @@
-#ifndef __D3D8HELPER_H__
-#define __D3D8HELPER_H__
+#ifndef __D3DHELPER_H__
+#define __D3DHELPER_H__
 
 #include <string>
 #include <windows.h>
@@ -7,9 +7,7 @@
 namespace Sexy
 {
 	bool GetD3D8AdapterInfo(GUID& theGUID, std::string& theDriver, std::string& theDescription);
-	bool GetD3D9AdapterInfo(GUID& theGUID, std::string& theDriver, std::string& theDescription);
-	bool GetD3D10AdapterInfo(GUID &theGUID, std::string &theDriver, std::string &theDescription);
-	bool GetD3D11AdapterInfo(GUID& theGUID, std::string& theDriver, std::string& theDescription);
+
 }
 
 #endif
